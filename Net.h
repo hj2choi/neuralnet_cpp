245d07add63b26a0306321b844ee2fcf5aafd3d2
hjchoi95 <hongjoonchoi95@gmail.com>
2016-05-03 14:55:58 +0800

    refactor: placed Nueron class into seperate file

--- Net.h
+++ Net.h
@@ -6,40 +6,10 @@
 #include <cassert>
 #include <cmath>
 #include <cstdlib>
-
+#include "Neuron.h"
 
 // activation function. it is currently implemented with sigmoid function
 double activationFunc(double net);
-
-
-// link, that goes out from neuron.
-// I had to realize that I don't really need this struct here.
-struct Link {
-	double weight;
-	double prevDeltaWeight;
-};
-
-
-// neuron class
-class Neuron {
-public:
-	double net; // net value on neuron
-	double out; // output value of neuron ( out = activationFunc(net) )
-	double deltaErr; // delta error value, for backward propagation
-	std::vector<Link> links;
-
-	// constructor for neuron: assign initial weight and create outgoing edges
-	Neuron(unsigned numLinks) {
-		for (int i=0; i<numLinks; ++i) {
-			Link tempConnection;
-			tempConnection.weight = (double)(std::rand()%100)/100;
-			links.push_back(tempConnection);
-			//std::cout << "created link for neuron" << std::endl;
-		}
-	}
-
-};
-
 
 class Net
 {
